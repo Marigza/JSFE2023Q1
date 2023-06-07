1c68a63a6a432c8cbee08b5b36358f1ba25bb1a4
MarinaGapeeva <marigola@mail.ru>
2023-06-07 14:42:36 +0300

    feat: add apiKey

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '05cb4bc4157d4c18b3ea67fbe736059f', // получите свой ключ https://newsapi.org/
         });
     }
 }
