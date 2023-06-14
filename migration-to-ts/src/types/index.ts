68fd743e164fe724dcd9589b225f0e58a6212e07
MarinaGapeeva <marigola@mail.ru>
2023-06-14 19:42:05 +0300

    feat: add interfaces to file

--- migration-to-ts/src/types/index.ts
+++ migration-to-ts/src/types/index.ts
@@ -5,17 +5,36 @@ export interface RequestParam {
   language?: 'ar' | 'de' | 'en' | 'es' | 'fr' | 'he' | 'it' | 'nl' | 'no' | 'pt' | 'ru' | 'sv' | 'ud' | 'zh';
   country?: 'ae' | 'ar' | 'at' | 'au' | 'be' | 'bg' | 'br' | 'ca' | 'ch' | 'cn' | 'co' | 'cu' | 'cz' | 'de' | 'eg' | 'fr' | 'gb' | 'gr' | 'hk' | 'hu' | 'id' | 'ie' | 'il' | 'in' | 'it' | 'jp' | 'kr' | 'lt' | 'lv' | 'ma' | 'mx' | 'my' | 'ng' | 'nl' | 'no' | 'nz' | 'ph' | 'pl' | 'pt' | 'ro' | 'rs' | 'ru' | 'sa' | 'se' | 'sg' | 'si' | 'sk' | 'th' | 'tr' | 'tw' | 'ua' | 'us' | 've' | 'za';
 }
-export interface ResponseObject {
+export interface ResponseArticles {
   status: string;
-  source: string[];
+  articles: Item[];
+}
+
+export interface ResponseSources {
+  status: string;
+  sources: SourceItem[];
 }
 
 export interface SourceItem {
-  id: string;
-  name: string;
-  description: string;
+  id: string; 
+  name: string; 
+  description: string; 
   url: string;
-  category: string;
-  language: string;
-  country: string
+  category: string; 
+  language: string; 
+  country: string; 
+}
+
+export interface Item {
+    urlToImage: string; 
+    author: string; 
+    content: string; 
+    source: {
+      id: string
+      name: string;
+    };
+    publishedAt: string; 
+    title: string; 
+    description: string; 
+    url: string; 
 }
