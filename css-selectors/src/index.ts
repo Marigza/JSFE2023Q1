58eb08e42c65479f1abd91a5e813f794cbd9904d
MarinaGapeeva <marigola@mail.ru>
2023-07-03 19:58:53 +0300

    refactor: add spases between blocks, fix path for import

--- css-selectors/src/index.ts
+++ css-selectors/src/index.ts
@@ -1,48 +1,54 @@
 import './index.html';
 import './style.scss';
-import { Table } from './script/render';
+import { Table } from './apps/table';
 
 const table = new Table();
 
 const buttonReset = document.getElementById('reset');
 const buttonHelp = document.getElementById('help');
+const buttonEnter = document.getElementById('enter');
 
 buttonReset?.addEventListener('click', () => table.resetProgress());
-buttonHelp?.addEventListener('click', () => table.showAnswer());
 
-console.log(buttonHelp);
+buttonHelp?.addEventListener('click', () => table.showAnswer());
 
 table.renderTable();
 table.renderLevels();
 
-const buttonEnter = document.getElementById('enter');
 buttonEnter?.addEventListener('click', () => {
   table.checkCode();
 });
-document.addEventListener('keydown', function(event) {
+
+document.addEventListener('keydown', function (event) {
+  
   if (event.code === 'Enter') {
     table.checkCode();
     buttonEnter?.classList.add('button_active');
+    
     setTimeout(() => buttonEnter?.classList.remove('button_active'), 300);
   }
+
 });
 
 function setLocalStorage() {
+
   if (table.level <= 10) {
     localStorage.setItem('level', String(table.level));
   } else {
     localStorage.setItem('level', '0');
   }
-  
+
 }
+
 window.addEventListener('beforeunload', setLocalStorage);
 
 function getLocalStorage() {
+
   if (localStorage.getItem('level')) {
-    //table.level = Number(localStorage.getItem('level'));
     return table.level;
   }
 }
+
 window.addEventListener('load', () => {
   getLocalStorage;
 });
