489862a9963e8cb1001e2ac2523de062ca8bd493
MarinaGapeeva <marigola@mail.ru>
2023-07-18 20:06:41 +0300

    feat: create class Request with getCars, getCar, createCar methods

---
+++ async-race/src/apps/serverRequest.ts
@@ -0,0 +1,35 @@
+import { newCar } from "./generateNewCar";
+
+//console.log(new newCar);
+
+export class Request {
+
+  async getCars() {
+  const response = await fetch('http://127.0.0.1:3000/garage');
+    const cars = await response.json();
+    console.log(cars.length);
+  return cars;
+  }
+  
+  async getCar(id: number) {
+    const response = await fetch(`http://127.0.0.1:3000/garage/${id}`);
+    const choiseCar = await response.json();
+    console.log(choiseCar);
+    return choiseCar;
+  }
+
+  async createCar(createdCar: newCar) {
+    const response = await fetch('http://127.0.0.1:3000/garage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(createdCar)
+    })
+    const result = await response.json();
+    return result;
+  }
+
+}
+
+
