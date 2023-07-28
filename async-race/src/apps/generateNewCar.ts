30f61a2209f412d8af708e4ac7a5f88c461d0170
MarinaGapeeva <marigola@mail.ru>
2023-07-28 17:12:29 +0300

    refactor: add field colorCodeLength to class newCar

--- async-race/src/apps/generateNewCar.ts
+++ async-race/src/apps/generateNewCar.ts
@@ -3,6 +3,7 @@ export class newCar {
   name: string;
   color: string;
   id: number;
+  colorCodeLength = 6;
   
   constructor() {
     this.name = this.createName();
@@ -15,21 +16,21 @@ export class newCar {
     const markArray = ['x1', 'z2', 'c4', 'y6', 'q3', 'f5', 'a6', 's7', 'y8', 'w4'];
     const modelIndex = Math.floor(Math.random() * modelArray.length);
     const markIndex = Math.floor(Math.random() * markArray.length);
+
     this.name = `${modelArray[modelIndex]}-${markArray[markIndex]}`;
+
     return this.name;
   }
 
   createColor() {
     const colorArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
     const readyColor = [];
-    while (readyColor.length <= 5) {
+
+    while (readyColor.length < this.colorCodeLength) {
       const colorIndex = Math.floor(Math.random() * colorArray.length);
       readyColor.push(colorArray[colorIndex]);
     }
+
     return `#${readyColor.join('')}`
   }
-
-  // async getCarId() {
-    
-  // }
 }
