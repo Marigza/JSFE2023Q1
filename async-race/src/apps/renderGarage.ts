51c925efbe75b2afcb003674392f2fe39764a316
MarinaGapeeva <marigola@mail.ru>
2023-08-12 09:39:25 +0300

    feat: create new file getSvgCar.ts

--- async-race/src/apps/renderGarage.ts
+++ async-race/src/apps/renderGarage.ts
@@ -2,6 +2,7 @@ import { BaseElement } from "./baseElement";
 import { Car } from "./car";
 import { Pagination } from "./pagination";
 import { Request, Winner } from "./serverRequest";
+import { getSvgCar } from "./utils/getSvgCar";
 
 export const mainBlock = new BaseElement({  tag: 'div', classlist: 'main-block', content: '' }).elem;
 
@@ -39,6 +40,8 @@ class CarTrackView{
   }
 
   render() {
+    const carSVG = getSvgCar(this.color);
+
     this.carView.append(this.carViewCheckView);
     this.carView.append(this.carViewTrack);
     this.carViewCheckView.append(this.carSelectButton);
@@ -52,8 +55,6 @@ class CarTrackView{
     this.carMoveStop.classList.add('button_car-view');
     this.carMoveStop.classList.add('disabled');
     this.carMoveStop.setAttribute('disabled', 'true')
-    
-    const carSVG = `<svg class="svg-car" fill="${this.color}" xmlns="http://www.w3.org/2000/svg" id="mdi-car-sports" viewBox="0 0 24 24"><path d="M12,8.5H7L4,11H3C1.89,11 1,11.89 1,13V16H3.17C3.6,17.2 4.73,18 6,18C7.27,18 8.4,17.2 8.82,16H15.17C15.6,17.2 16.73,18 18,18C19.27,18 20.4,17.2 20.82,16H23V15C23,13.89 21.97,13.53 21,13L12,8.5M5.25,12L7.5,10H11.5L15.5,12H5.25M6,13.5A1.5,1.5 0 0,1 7.5,15A1.5,1.5 0 0,1 6,16.5A1.5,1.5 0 0,1 4.5,15A1.5,1.5 0 0,1 6,13.5M18,13.5A1.5,1.5 0 0,1 19.5,15A1.5,1.5 0 0,1 18,16.5A1.5,1.5 0 0,1 16.5,15A1.5,1.5 0 0,1 18,13.5Z" /></svg>`;
     this.carViewTrack.innerHTML = `<div>${carSVG}</div>`;
     this.activeCar = this.carViewTrack.firstElementChild as HTMLDivElement;
   }
@@ -329,7 +330,6 @@ export class Garage {
 
     alert('Please, wait some moments...');
     
-
     for (let i = 0; i < numberOfGeneratedCars; i++) {
       await this.showNewCarOnGarage();
     }
